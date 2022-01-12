1df3cb132fb7fe27decbb50f08b8049379dfd1a7
mikitaro-27 <matt.diak@gmail.com>
2022-01-12 16:05:00 +0000

    STEP 11 - page show 'Title: url'

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -5,8 +5,12 @@ class Bookmark
   def self.all
     bookmarks = Database.select_all('bookmark_manager', 'bookmarks')
     # bookmark_row is {"id"=>"25", "url"=>"URL", "title"=>"TITLE"}
-    bookmarks.map { |bookmark_row|  bookmark_row['url'] + ' ' + bookmark_row['title'] }
+    # we have a hash, we want to see title as a separate string from the url on the page
+    bookmarks.map { |bookmark_row|  "#{bookmark_row['title']}: #{bookmark_row['url']}" }
     #  ['url'] + ' ' + bookmark_row['title']
+
+    # {"id"=>"25", "url"=>"URL", "title"=>"TITLE"}
+  
   end
     
   def self.add(url, title)
--- spec/features/save_bookmark_spec.rb
+++ spec/features/save_bookmark_spec.rb
@@ -4,15 +4,16 @@ feature 'saving bookmark' do
     # in this test when we save a bookmark we want to see the bookmark in the bookmarks list
     visit '/bookmarks'
     fill_in('page_address', with: 'https://www.cloudflare.com/') #put down the address
+    fill_in('title', with: 'Test Title')
     click_on "Add bookmark" #click some add button
-    expect(page).to have_content 'https://www.cloudflare.com/'
+    expect(page).to have_content 'Test Title: https://www.cloudflare.com/'
   end
 
-  scenario 'a title column appears within the table' do
-    visit '/bookmarks'
-    fill_in('page_address', with: 'https://www.cloudflare.com/')
-    fill_in('title', with: 'Test Title')
-    click_on "Add bookmark"
-    expect(page).to have_content 'Test Title'
-  end
+  # scenario 'a title column appears within the table' do
+  #   visit '/bookmarks'
+  #   fill_in('page_address', with: 'https://www.cloudflare.com/')
+  #   fill_in('title', with: 'Test Title')
+  #   click_on "Add bookmark"
+  #   expect(page).to have_content 'Test Title'
+  # end
 end
